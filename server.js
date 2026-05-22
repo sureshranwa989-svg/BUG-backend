1b500bef3f3b4ccdb7c6459b27d566e3e6bcb38a
Suresh Ranwa <sureshranwa@192.168.1.32>
2026-05-22 18:31:38 +0530

    resolve default route

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-require ('dotenv').config();
-const app = require('./src/app')
-const connectDB = require('./src/db/db')
+require("dotenv").config();
 
-connectDB()
+const app = require("./src/app");
+const connectDB = require("./src/db/db");
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+// Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on port http://localhost:${PORT}`)
-} );
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
